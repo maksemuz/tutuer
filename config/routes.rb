9b59166a9b8e791ab677683a72ce285d0e4547ea
MMC <maksemuz@gmail.com>
2017-04-25 05:54:22 +0300

    task 12

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'tutuer#index'
   resources :trains
   resources :railway_stations
+  resources :routes
   get 'railway_stations/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
---
+++ db/migrate/20170424195452_routes.rb
@@ -0,0 +1,9 @@
+class Routes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :routes do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
