9868a8d8b97898d586e088d1a3230741172a7223
MMC <maksemuz@gmail.com>
2017-05-04 15:34:59 +0300

    select in wagons fixed

--- app/models/train.rb
+++ app/models/train.rb
@@ -7,7 +7,7 @@ class Train < ApplicationRecord
   validates :number, presence: true
 
   def wagons_logic(kind)
-    w = self.wagons.select {|w| w if w.wagon_kind == kind}
+    w = self.wagons.select {|w| w.wagon_kind == kind}
     {
       size: w.size,
       upper: w.sum(&:upper_places),
