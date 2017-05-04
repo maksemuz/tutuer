f2686962e44e8a68d47f41851cdedd4f295d0d2f
MMC <maksemuz@gmail.com>
2017-05-04 06:41:40 +0300

    add wagon places validators

--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -2,4 +2,6 @@ class Wagon < ApplicationRecord
   belongs_to :train
 
   validates :number, presence: true
+  validates :upper_places,numericality: { greater_than: 17, less_than: 28 }
+  validates :lower_places,numericality: { greater_than: 17, less_than: 28 }
 end
