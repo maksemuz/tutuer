5200bd38c7734ecac5885da5246be654e6810fdf
MMC <maksemuz@gmail.com>
2017-06-03 11:09:34 +0300

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
       port:                 587,
       domain:               'thawing-journey-48790.herokuapp.com',
       user_name:            'masterstakanov',
-      password:             'CENSORED',
+      password:             '1q@W3e$R5t',
       authentication:       'plain',
       enable_starttls_auto: true  }
 end
