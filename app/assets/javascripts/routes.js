cd1f147792c8f4df3f24ba8c5a3aaa4916543ef0
MMC <maksemuz@gmail.com>
2017-06-07 18:13:13 +0300

    task 20 fixes

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,13 +1,9 @@
 $(document).on('turbolinks:load', function () {
     $('a.edit_route').click(function (e) {
         e.preventDefault();
-        var route_id;
-        var form;
-        var name;
-
-        route_id = $(this).data('routeId');
-        form = $('#edit_route_' + route_id);
-        name = $('#route_name_' + route_id);
+        var route_id = $(this).data('routeId');
+        var form = $('#edit_route_' + route_id);
+        var name = $('#route_name_' + route_id);
 
         if (!$(this).hasClass('cancel')) {
             $(this).html('Отменить');
